0cca73888369b21443dc4faea87a85970f960c1c
Serene Han <keroserene+git@gmail.com>
2015-12-31 17:01:06 -0800

    convert Data's channel_test to goconvey

--- data/channel_test.go
+++ data/channel_test.go
@@ -1,156 +1,122 @@
 package data
 
 import (
-	// "fmt"
-	"reflect"
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 	"time"
 )
 
-var c *Channel
-
-func checkEnum(t *testing.T, desc string, enum int, expected int) {
-	if enum != expected {
-		t.Error("Mismatched Enum Value -", desc,
-			"\nwas:", enum,
-			"\nexpected:", expected)
-	}
-}
-
 func TestDataStateEnums(t *testing.T) {
-	checkEnum(t, "DataStateConnecting",
-		int(DataStateConnecting), _cgoDataStateConnecting)
-	checkEnum(t, "DataStateOpen",
-		int(DataStateOpen), _cgoDataStateOpen)
-	checkEnum(t, "DataStateClosing",
-		int(DataStateClosing), _cgoDataStateClosing)
-	checkEnum(t, "DataStateClosed",
-		int(DataStateClosed), _cgoDataStateClosed)
-}
 
-func TestNewChannel(t *testing.T) {
-	c = NewChannel(cgoFakeDataChannel())
-	if nil == c {
-		t.Fatal("Could not create NewChannel")
-	}
-}
+	Convey("Enum: DataState", t, func() {
+		So(DataStateConnecting, ShouldEqual, _cgoDataStateConnecting)
+		So(DataStateOpen, ShouldEqual, _cgoDataStateOpen)
+		So(DataStateClosing, ShouldEqual, _cgoDataStateClosing)
+		So(DataStateClosed, ShouldEqual, _cgoDataStateClosed)
+	})
 
-// There's not a good way to create a DataChannel without first having
-// an available PeerConnection object with a valid session, but that's part of
-// the outer package, so testing the attributes is less useful here.
+	Convey("DataChannel", t, func() {
 
-func TestChannelLabel(t *testing.T) {
-	if "fake" != c.Label() {
-		t.Error()
-	}
-}
+		c := NewChannel(cgoFakeDataChannel())
+		So(c, ShouldNotBeNil)
 
-func TestChannelOrdered(t *testing.T) {
-	if false != c.Ordered() {
-		t.Error()
-	}
-}
+		// There's not a good way to create a DataChannel without first having an
+		// available PeerConnection object with a valid session, but that's part of
+		// the outer package, so testing the attributes is less useful here.
+		Convey("Callbacks should fire correctly", func() {
 
-func TestChannelReadyState(t *testing.T) {
-	if DataStateClosed != c.ReadyState() {
-		t.Error()
-	}
-}
+			Convey("OnMessage", func() {
+				success := make(chan []byte, 1)
+				c.OnMessage = func(msg []byte) {
+					success <- msg
+				}
+				bytes := []byte("somenumberofbytesinhere")
+				size := len(bytes)
+				cgoFakeMessage(c, bytes, size)
+				select {
+				case data := <-success:
+					So(c.OnMessage, ShouldNotBeNil)
+					So(data, ShouldResemble, bytes)
+				case <-time.After(time.Second * 1):
+					t.Fatal("Timed out.")
+				}
+			})
 
-func TestOnMessageCallback(t *testing.T) {
-	success := make(chan []byte, 1)
-	c.OnMessage = func(msg []byte) {
-		success <- msg
-	}
-	bytes := []byte("somenumberofbytesinhere")
-	size := len(bytes)
-	cgoFakeMessage(c, bytes, size)
-	select {
-	case data := <-success:
-		if !reflect.DeepEqual(data, bytes) {
-			t.Error("Unexpected bytes: ", data)
-		}
-	case <-time.After(time.Second * 1):
-		t.Fatal("Timed out.")
-	}
-}
+			Convey("StateChangeCallbacks", func() {
+				opened := make(chan int, 1)
+				closed := make(chan int, 1)
+				c.OnOpen = func() {
+					opened <- 1
+				}
+				c.OnClose = func() {
+					closed <- 1
+				}
 
-func TestStateChangeCallbacks(t *testing.T) {
-	opened := make(chan int, 1)
-	closed := make(chan int, 1)
-	c.OnOpen = func() {
-		opened <- 1
-	}
-	c.OnClose = func() {
-		closed <- 1
-	}
+				cgoFakeStateChange(c, DataStateOpen)
+				select {
+				case <-opened:
+					So(c.OnOpen, ShouldNotBeNil)
+					So(c.ReadyState(), ShouldEqual, DataStateOpen)
+				case <-time.After(time.Second * 1):
+					t.Fatal("Timed out when waiting for Open.")
+				}
 
-	cgoFakeStateChange(c, DataStateOpen)
-	select {
-	case <-opened:
-		if DataStateOpen != c.ReadyState() {
-			t.Error("Unexpected state: ", c.ReadyState())
-		}
-	case <-time.After(time.Second * 1):
-		t.Fatal("Timed out when waiting for Open.")
-	}
+				cgoFakeStateChange(c, DataStateClosed)
+				select {
+				case <-closed:
+					So(c.OnClose, ShouldNotBeNil)
+					So(c.ReadyState(), ShouldEqual, DataStateClosed)
+				case <-time.After(time.Second * 1):
+					t.Fatal("Timed out when waiting for Closed.")
+				}
+			})
 
-	cgoFakeStateChange(c, DataStateClosed)
-	select {
-	case <-closed:
-		if DataStateClosed != c.ReadyState() {
-			t.Error("Unexpected state: ", c.ReadyState())
-		}
-	case <-time.After(time.Second * 1):
-		t.Fatal("Timed out when waiting for Closed.")
-	}
-	// Set to open for the next tests.
-	cgoFakeStateChange(c, DataStateOpen)
-}
+			Convey("OnBufferedAmountLow", func() {
+				success := make(chan int, 1)
+				c.BufferedAmountLowThreshold = 100
+				c.OnBufferedAmountLow = func() {
+					success <- 1
+				}
+				cgoFakeBufferAmount(c, 90)
+				select {
+				case <-success:
+					So(c.OnBufferedAmountLow, ShouldNotBeNil)
+				case <-time.After(time.Second * 1):
+					t.Fatal("Timed out.")
+				}
+			})
+		})
 
-func TestOnBufferedAmountLowCallback(t *testing.T) {
-	success := make(chan int, 1)
-	c.BufferedAmountLowThreshold = 100
-	c.OnBufferedAmountLow = func() {
-		success <- 1
-	}
-	cgoFakeBufferAmount(c, 90)
-	select {
-	case <-success:
-	case <-time.After(time.Second * 1):
-		t.Fatal("Timed out.")
-	}
-}
+		Convey("Send", func() {
+			messages := make(chan []byte, 1)
+			data := []byte("some data to send")
+			c.OnMessage = func(msg []byte) {
+				messages <- msg
+			}
+			c.Send(data)
+			select {
+			case recv := <-messages:
+				So(c.OnMessage, ShouldNotBeNil)
+				So(recv, ShouldResemble, data)
+			case <-time.After(time.Second * 1):
+				t.Fatal("Timed out.")
+			}
+		})
 
-func TestSend(t *testing.T) {
-	messages := make(chan []byte, 1)
-	data := []byte("some data to send")
-	c.OnMessage = func(msg []byte) {
-		messages <- msg
-	}
-	c.Send(data)
-	select {
-	case recv := <-messages:
-		if !reflect.DeepEqual(recv, data) {
-			t.Error("Unexpected bytes: ", recv)
-		}
-	case <-time.After(time.Second * 1):
-		t.Fatal("Timed out.")
-	}
-}
+		Convey("Close", func() {
+			closed := make(chan int, 1)
+			c.OnClose = func() {
+				closed <- 1
+			}
+			c.Close()
+			select {
+			case <-closed:
+				So(c.OnClose, ShouldNotBeNil)
+				So(c.ReadyState(), ShouldEqual, DataStateClosed)
+			case <-time.After(time.Second * 1):
+				t.Fatal("Timed out during close..")
+			}
+		})
+	})
 
-func TestCloseChannel(t *testing.T) {
-	closed := make(chan int, 1)
-	c.OnClose = func() {
-		closed <- 1
-	}
-	c.Close()
-	select {
-	case <-closed:
-		if DataStateClosed != c.ReadyState() {
-			t.Error("Unexpected state: ", c.ReadyState())
-		}
-	case <-time.After(time.Second * 1):
-		t.Fatal("Timed out during close..")
-	}
 }
