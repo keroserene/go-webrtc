f3be1ff72ca24eca2eb17079bbc95322806eac7c
Ximin Luo <infinity0@pwned.gg>
2016-01-27 01:39:45 +0100

    Use C++11 which is the default on Debian unstable
    Note that we can't just remove the cgo CXXFLAGS, the build fails when we do
    that. It seems that cgo sets a std *different* from what the system default
    compiler does when compiling plain C++ code. :/

--- peerconnection.go
+++ peerconnection.go
@@ -30,7 +30,7 @@ Please share any improvements or concerns as issues or pull requests on github.
 package webrtc
 
 /*
-#cgo CXXFLAGS: -std=c++0x
+#cgo CXXFLAGS: -std=c++11
 #cgo linux,amd64 pkg-config: webrtc-linux-amd64.pc
 #cgo darwin,amd64 pkg-config: webrtc-darwin-amd64.pc
 #include <stdlib.h>  // Needed for C.free
