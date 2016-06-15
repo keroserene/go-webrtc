5e9969c3bf5e63df086566404241c06279e5d56a
Serene Han <keroserene+git@gmail.com>
2016-06-15 12:07:13 -0700

    use mutex with CGOMap mutation

--- utils.go
+++ utils.go
@@ -1,5 +1,7 @@
 package webrtc
 
+import "sync"
+
 // A general map that stores pointers by an unique int index, since we can't
 // keep Go pointers in C land.  This is a little workaround suggested in the
 // proposal for the cgo rules as of 1.6
@@ -7,11 +9,13 @@ package webrtc
 type CGOMap struct {
 	index    int
 	pointers map[int]interface{}
+	lock     *sync.Mutex
 }
 
 func NewCGOMap() CGOMap {
 	var m CGOMap
 	m.pointers = make(map[int]interface{})
+	m.lock = &sync.Mutex{}
 	return m
 }
 
@@ -24,11 +28,15 @@ func (m *CGOMap) Get(index int) interface{} {
 }
 
 func (m *CGOMap) Set(p interface{}) int {
+	m.lock.Lock()
 	m.index += 1
 	m.pointers[m.index] = p
+	m.lock.Unlock()
 	return m.index
 }
 
 func (m *CGOMap) Delete(index int) {
+	m.lock.Lock()
 	delete(m.pointers, index)
+	m.lock.Unlock()
 }
