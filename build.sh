5dc5bb5369f9a7530cc77979582db89162344d05
Cant Stop The Signal <cantstopthesignals@gmail.com>
2016-12-04 18:46:06 -0800

    Update build.sh for repeatable builds of libwebrtc-magic.a
    Modify build.sh to perform a mostly self contained build of webrtc at
    the tested commit id. Some pre-requisites must be installed before or
    after a missing tool failure is encountered, e.g. pkg-config on mac or
    webrtc/src/build/install-build-deps.sh on linux.
    
    Tested: linux and mac builds worked from clean virtual machines. Demos
    and tests passed.

--- build.sh
+++ build.sh
@@ -5,33 +5,55 @@ THIRD_PARTY_DIR="$PROJECT_DIR/third_party"
 WEBRTC_REPO="https://chromium.googlesource.com/external/webrtc"
 WEBRTC_DIR="$THIRD_PARTY_DIR/webrtc"
 WEBRTC_SRC="$WEBRTC_DIR/src"
+DEPOT_TOOLS_DIR="$THIRD_PARTY_DIR/depot_tools"
 OS=$(go env GOOS)
 ARCH=$(go env GOARCH)
-CONFIG="Debug"
+CONFIG="Release"
 COMMIT="f33698296719f956497d2dbff81b5080864a8804"  # branch-heads/52
 
 INCLUDE_DIR="$PROJECT_DIR/include"
 LIB_DIR="$PROJECT_DIR/lib"
 
-# TODO(arlolra): depot_tools
-
-GYP_DEFINES="include_tests=0"
+PATH="$PATH:$DEPOT_TOOLS_DIR"
 
 mkdir -p $THIRD_PARTY_DIR
 mkdir -p $INCLUDE_DIR
 mkdir -p $LIB_DIR
 
+if [[ -d $DEPOT_TOOLS_DIR ]]; then
+	echo "Syncing depot_tools ..."
+	pushd $DEPOT_TOOLS_DIR
+	git pull --rebase || exit 1
+	popd
+else
+	echo "Getting depot_tools ..."
+	mkdir -p $DEPOT_TOOLS_DIR
+	git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git $DEPOT_TOOLS_DIR || exit 1
+fi
+
 if [[ -d $WEBRTC_DIR ]]; then
-	echo "Sync'ing webrtc ..."
+	echo "Syncing webrtc ..."
+	pushd $WEBRTC_SRC || exit 1
+	if ! git diff-index --quiet HEAD --; then
+		echo -en "\nOpen files present in $WEBRTC_SRC\nReset them? (y/N): "
+		read ANSWER
+		if [ "$ANSWER" != "y" ]; then
+			echo "*** Cancelled ***"
+			exit 1
+		fi
+		git reset --hard HEAD || exit 1
+	fi
+	popd
+
 	pushd $WEBRTC_DIR
-	gclient sync
+	gclient sync --with_branch_heads -r $COMMIT || exit 1
 	popd
 else
 	echo "Getting webrtc ..."
 	mkdir -p $WEBRTC_DIR
 	pushd $WEBRTC_DIR
-	gclient config --name src $WEBRTC_REPO
-	gclient sync
+	gclient config --name src $WEBRTC_REPO || exit 1
+	gclient sync --with_branch_heads -r $COMMIT || exit 1
 	popd
 fi
 
@@ -40,34 +62,51 @@ pushd $WEBRTC_SRC
 git checkout $COMMIT
 popd
 
-echo "Generating build scripts ..."
-pushd $WEBRTC_SRC
-python webrtc/build/gyp_webrtc
+echo "Cleaning webrtc ..."
+pushd $WEBRTC_SRC || exit 1
+rm -rf out/$CONFIG
+popd
+
+echo "Applying webrtc patches ..."
+pushd $WEBRTC_SRC || exit 1
+for PATCH in build_at_webrtc_branch_heads_52.patch; do
+	git apply --check ${PROJECT_DIR}/webrtc_patches/${PATCH} || exit 1
+	git am < ${PROJECT_DIR}/webrtc_patches/${PATCH} || exit 1
+done
 popd
 
 echo "Building webrtc ..."
 pushd $WEBRTC_SRC
-ninja -C out/$CONFIG
+export GYP_DEFINES="include_tests=0 include_examples=0"
+python webrtc/build/gyp_webrtc webrtc/api/api.gyp || exit 1
+ninja -C out/$CONFIG || exit 1
 popd
 
 echo "Copying headers ..."
-pushd $WEBRTC_SRC
+pushd $WEBRTC_SRC || exit 1
+rm -rf "$INCLUDE_DIR"
 for h in $(find webrtc/ -type f -name '*.h')
 do
 	mkdir -p "$INCLUDE_DIR/$(dirname $h)"
 	cp $h "$INCLUDE_DIR/$h"
 done
 popd
+pushd $PROJECT_DIR || exit 1
+git clean -fdx "$INCLUDE_DIR"
+popd
 
 echo "Concatenating libraries ..."
 pushd $WEBRTC_SRC/out/$CONFIG
-# on osx:
-# ls *.a > filelist
-# libtool -static -o libwebrtc-magic.a -filelist filelist
-# strip -S -x -o libwebrtc-magic.a libwebrtc-magic.a
-# on linux:
-# ar crs libwebrtc-magic.a $(find . -name '*.o' -not -name '*.main.o')
-mv libwebrtc-magic.a $LIB_DIR/libwebrtc-$OS-$ARCH-magic.a
+if [ "$OS" = "darwin" ]; then
+	ls *.a > filelist
+	libtool -static -o libwebrtc-magic.a -filelist filelist
+	strip -S -x -o libwebrtc-magic.a libwebrtc-magic.a
+else
+	ar crs libwebrtc-magic.a $(find . -name '*.o' -not -name '*.main.o' -not -name 'dump_syms_regtest.o')
+fi
+OUT_LIBRARY=$LIB_DIR/libwebrtc-$OS-$ARCH-magic.a
+mv libwebrtc-magic.a ${OUT_LIBRARY}
+echo "Built ${OUT_LIBRARY}"
 popd
 
 echo "Build complete."
