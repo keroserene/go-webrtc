697b5b961201d44580a4c2aee8da1156abc4d4bd
David Fifield <david@bamsoftware.com>
2018-11-30 22:05:32 -0700

    Fix build.sh usage examples.
    They had GOOS twice, instead of GOOS and GOARCH.

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 # For a native compile (using current values of GOOS and GOARCH):
 #   ./build.sh
 # For a cross compile:
-#   GOOS=linux GOOS=amd64 ./build.sh
-#   GOOS=linux GOOS=arm ./build.sh
-#   GOOS=darwin GOOS=amd64 ./build.sh
+#   GOOS=linux GOARCH=amd64 ./build.sh
+#   GOOS=linux GOARCH=arm ./build.sh
+#   GOOS=darwin GOARCH=amd64 ./build.sh
 # (For a cross-compile from linux-amd64 to linux-arm, you may need to install the binutils-arm-linux-gnueabihf package.)
 
 PROJECT_DIR=$(pwd)
